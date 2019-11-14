afee98eadce07afb09534891e0cf388ff63c3323
airycanine <kubawalat@gmail.com>
2019-11-14 19:59:49 +0100

    Implemented speech to text

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,39 @@
-import React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { makeStyles } from "@material-ui/core/styles";
 
-import Player from './components/Player/Player';
-import Playlist from './components/Playlist/Playlist';
+import Player from "./components/Player/Player";
+import Playlist from "./components/Playlist/Playlist";
+import useAudio from "./hooks/useAudio";
+import AudioInputListener from "./components/AudioInputListener/AudioInputListener";
 
 const useStyles = makeStyles({
   application: {
-    width: '100vw',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-around',
-    height: '100vh',
-  },
+    width: "100vw",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-around",
+    height: "100vh"
+  }
 });
+
+const audioUrl = "http://www.ne.jp/asahi/music/myuu/wave/hana.mp3";
+
+const AudioPlayer = () => {
+  const [playing, currentTime, play, pause, jump] = useAudio(audioUrl);
+
+  return (
+    <>
+      <p>currenttime: {currentTime}</p>
+
+      <button onClick={playing ? pause : play}>
+        {playing ? "Pause" : "Play"}
+      </button>
+
+      <button onClick={() => jump(30)}>30sec ▶︎</button>
+    </>
+  );
+};
 
 function App() {
   const { application } = useStyles();
@@ -22,7 +42,8 @@ function App() {
     <div className={application}>
       <CssBaseline />
       <Player />
-      Na brode merlina
+      {/* <AudioPlayer/> */}
+      <AudioInputListener />
       <Playlist />
     </div>
   );
